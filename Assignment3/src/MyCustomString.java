6afc14ba351924455d8c0129a0ace92d19444432
Shashi Sasitharan <Shashi.Sasitharan@gmail.com>
2017-10-01 01:48:14 -0400

    Third commit, project start

--- Assignment3/src/MyCustomString.java
+++
@@ -1,7 +0,0 @@
-public class MyCustomString {
-
-    public static void main()
-    {
-        System.out.print("Test");
-    }
-}
---
+++ Assignment3/src/edu/qc/seclass/MyCustomString.java
@@ -0,0 +1,29 @@
+package edu.qc.seclass;
+
+public class MyCustomString implements MyCustomStringInterface {
+
+    @Override
+    public String getString() {
+        return null;
+    }
+
+    @Override
+    public void setString(String string) {
+
+    }
+
+    @Override
+    public int countNumbers() {
+        return 0;
+    }
+
+    @Override
+    public String getEveryNthCharacterFromBeginningOrEnd(int n, boolean startFromEnd) {
+        return null;
+    }
+
+    @Override
+    public void convertDigitsToNamesInSubstring(int startPosition, int endPosition) {
+
+    }
+}
